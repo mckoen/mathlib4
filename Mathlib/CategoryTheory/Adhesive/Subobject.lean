9b38296896757aed0e3f294c7454b1d61d8490ad
Jack McKoen <104791831+mckoen@users.noreply.github.com>
2026-02-05 19:08:44 +0000

    dagur review 2

--- Mathlib/CategoryTheory/Adhesive/Subobject.lean
+++ Mathlib/CategoryTheory/Adhesive/Subobject.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2026 Jack McKoen~. All rights reserved.
+Copyright (c) 2026 Jack McKoen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jack McKoen
 -/
